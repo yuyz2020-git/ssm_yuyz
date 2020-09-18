16dcbfa0ee2a050534506de2ed4ce04344564f59
yuyz2020-git <2568144650@qq.com>
2020-09-18 23:50:40 +0800

    修改步骤

--- src/test/java/com/yuyz/tests/MybatisMBGTest.java
+++ src/test/java/com/yuyz/tests/MybatisMBGTest.java
@@ -18,16 +18,16 @@ import java.util.List;
  * @Version 1.0
  */
 public class MybatisMBGTest {
-    @Test
-    public void test() throws Exception {
-        List<String> warnings = new ArrayList<String>();
-        boolean overwrite = true;
-        File configFile = new File("generatorConfig.xml");
-        ConfigurationParser cp = new ConfigurationParser(warnings);
-        Configuration config = cp.parseConfiguration(configFile);
-        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
-        myBatisGenerator.generate(null);
-    }
+//    @Test
+//    public void test() throws Exception {
+//        List<String> warnings = new ArrayList<String>();
+//        boolean overwrite = true;
+//        File configFile = new File("generatorConfig.xml");
+//        ConfigurationParser cp = new ConfigurationParser(warnings);
+//        Configuration config = cp.parseConfiguration(configFile);
+//        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+//        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+//        myBatisGenerator.generate(null);
+//    }
 
 }
